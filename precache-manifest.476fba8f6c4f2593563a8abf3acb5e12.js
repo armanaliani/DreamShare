c2230dbe7d44e30c738379fad54613fb67158803
armanaliani <61896630+armanaliani@users.noreply.github.com>
2022-07-26 13:58:05 -0400

    Updates

---
+++ precache-manifest.476fba8f6c4f2593563a8abf3acb5e12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc68abee4c0b00d748d825e7bf190cfb",
+    "url": "/dreamshare/index.html"
+  },
+  {
+    "revision": "7da28439b5ac3fb16c3a",
+    "url": "/dreamshare/static/css/main.420a6164.chunk.css"
+  },
+  {
+    "revision": "e5616b6ecc08619bf094",
+    "url": "/dreamshare/static/js/2.efca8e47.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/dreamshare/static/js/2.efca8e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da28439b5ac3fb16c3a",
+    "url": "/dreamshare/static/js/main.e44b09e0.chunk.js"
+  },
+  {
+    "revision": "25fa9c0be285c635de81",
+    "url": "/dreamshare/static/js/runtime-main.2033bb94.js"
+  },
+  {
+    "revision": "98b985073f61048c81900e607c829038",
+    "url": "/dreamshare/static/media/dreamShareAkatsukiCloud.98b98507.png"
+  },
+  {
+    "revision": "84058aa5ac6ae5fe4a8c5a549aee3dc3",
+    "url": "/dreamshare/static/media/wallpaperbetter.84058aa5.jpg"
+  }
+]);
